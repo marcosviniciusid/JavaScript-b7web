66b253d89320165126820f3d238c0af09f4d15bf
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-12 18:00:22 -0300

    metodos-array-3

---
+++ 26-metodos-de-array-3/script.js
@@ -0,0 +1,26 @@
+let lista = ['ovo', 'Farinha', 'Corante', 'Massa'];
+
+delete lista[1];
+
+lista.splice(1, 1);
+// 1Parametro = Remover a partir do item 1
+// 2Parametro = Quantidade de itens a remover
+// Se deixar só 1, remove tudo.
+
+let lista2 = ['Prato', 'Liquidificador', 'Forno'];
+
+let res = lista.concat(lista2);
+// Vai juntar as duas listas
+
+lista.sort();
+// Ordena o texto em ordem alfabetica crescente
+
+lista.sort();
+lista.reverse();
+// Primeiro ordena em ordem alfabetica e depois inverte com o reverse
+
+
+
+let res = lista;
+
+console.log(res);
